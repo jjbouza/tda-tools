b50030e9add8e2d732f0681a7180bb530aa0595f
Jose Bouza <josejbouza@gmail.com>
2018-06-13 13:32:07 -0400

    Fixed benchmarks.

--- examples/rip_benchmark.R
+++ examples/rip_benchmark.R
@@ -1,23 +1,19 @@
 library('tdatools')
 
-x<-sample(1:200, 200)
-y<-sample(1:200, 200)
+x<-sample(1:2000, 2000)
+y<-sample(1:2000, 2000)
 
 point_mat <- matrix(c(x,y),nrow=length(x))
 
 start_time <- Sys.time()
-for(i in 1:2000){
-	pd <- rip_pc(point_mat, 2, 2, 2, 0)
-}
+pd <- rip_pc(point_mat, 2, 2, 2, 0)
 end_time <- Sys.time()
 
 print(end_time-start_time)
 
 source("ripser_wrapper.R")
 start_time <- Sys.time()
-for(i in 1:2000){
-	pd2 <- ripser_wrapper(point_mat, "point-cloud", 2, 2)
-}
+pd2 <- ripser_wrapper(point_mat, "point-cloud", 2, 2)
 end_time <- Sys.time()
 
 print(end_time-start_time)
