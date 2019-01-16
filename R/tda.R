bbeb980520f6502dd48d035bed2c8b3765474e67
Jose Bouza <josejbouza@gmail.com>
2019-01-16 12:24:43 -0500

    Added initial logic for automatic threshold deduction in landscape function. Should be backwards compatible, but need to test thoroughly.

--- R/tda.R
+++ R/tda.R
@@ -85,14 +85,51 @@ PDplot <- function(PersistenceDiagram){
 
 #' Compute landscape of a persistence diagram.
 #'
-#' @param PersistenceDiagram A list of persistence pairs.
+#' @param PersistenceDiagram A pd object from the diagram function, or a list of pairs.
 #' @param exact Set to TRUE for exact computation, FALSE for discrete.
 #' @param max_x Domain threshold for discrete PL.
 #' @param dx Domain grid diameter for discrete PL.
+#' @param degree If input is pd object then degree specifies which degree to select from.
 #' @return A PersistenceLandscape object.
-landscape <- function(PersistenceDiagram, exact=FALSE, dx=0.1,  min_x= 0, max_x=10, max_y=1000){
+landscape <- function(PersistenceDiagram, degree=NULL, exact=FALSE, dx=0.1,  min_x=0, max_x=10, max_y=1000){
+
+    diagram = NULL
+    max_y = NULL
+
+    
+    #Automatic parameter deduction logic:
+
+    # Input is bd pairs (no threshold info).
+    if(is.null(PersistenceDiagram$param.threshold)){
+        diagram = PersistenceDiagram
+        
+        #We check if diagram has max death value < infinity:
+        if(max(diagram[,2]) < Inf){
+            max_y = max(diagram[,2])
+        }
+        
+        else{
+            max_y = max_y
+        }
+    }
+    
+    # Input is from diagram output.
+    else{
+        if(is.null(degree)){
+            stop("Error: If input persistence diagram is directly from 
+                 the diagram fucntion then a homological degree degree must be specified (add degree=__ to landscape call). ")
+            
+        }
+
+        else{
+            diagram = PersistenceDiagram$pairs[[degree]]
+            max_y = PersistenceDiagram$param.threshold
+        }
+    }
+    
+
 	#Construct a persistence landscape.
-	landscape_raw <- new(PersistenceLandscape, PersistenceDiagram, exact, min_x, max_x, dx, max_y)
+	landscape_raw <- new(PersistenceLandscape, diagram, exact, min_x, max_x, dx, max_y)
 }
 
 
---
+++ examples/auto-param.R
@@ -0,0 +1,14 @@
+library('tdatools')
+source('torus_sample.R')
+
+print('Sampling points...')
+points = TorusUnif(100,1,5)
+
+print('Computing PD...')
+#Compute persistence:
+pd <- diagram(points, 'point-cloud', dim_max=2,threshold=1)
+
+print('Computing PL for 1-dim cycles...')
+pl <- landscape(pd, degree=1, exact=TRUE, max_x=2, dx=0.1)
+
+print(pl$getInternal()[1,,])
